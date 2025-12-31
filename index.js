93db4f77e852d8c48c1d9851a8c35c118af463b4
Terra Fantastic <ethenpickett@gmail.com>
2025-12-31 17:03:18 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,36 @@
 // Load in Json file 
-fetch("Birds.json")
+fetch("info.json")
   .then(response => response.json())
   .then(json => {
 
     // Attach loaded in object to variable "birds"
-    const birds = json
-
-    // Function for displaying bird information when selected
-    function showbirdinfo(selected) {
-
-      // Clear all elements from the infowindow div
-      document.querySelector('#infowindow').innerHTML = ""
-
-      // Find data for selected bird species
-      birds.NativeBirds.forEach(element => {
-        if (element.Name == selected) {
-          
-          // Create heading with the bird name
-          let birdname = document.createElement('h3')
-          birdname.innerHTML = element.Name
-          document.querySelector('#infowindow').append(birdname)
-
-          // Display bird image
-          let birdimage = document.createElement('img')
-          birdimage.src = `pictures/${element.Name}.jpg`
-          birdimage.style.width = "20vw"
-          birdimage.style.height = "20vw"
-          document.querySelector('#infowindow').append(birdimage)
-
-          // Create a back button
-          let backbutton = document.createElement('button')
-          backbutton.innerHTML = "Change Selected Species"
-          backbutton.id = "backbutton"
-          document.querySelector('#infowindow').append(backbutton)
-          document.querySelector('#infowindow').append(document.createElement('br'))
-          document.getElementById("backbutton").addEventListener('click', () => {
-          createbirdbuttons(birds) })
-          
-          // Display species information
-          let newinfowindow = document.createElement('p')
-          newinfowindow.innerHTML = element.Desc
-          document.querySelector('#infowindow').append(newinfowindow)
-        }
-      })
-    }
+    const info = json
     
     // Function for displaying all bird options when no specific species is selected 
-    function createbirdbuttons(buttons) {
+    function setInitialView(info) {
+      
+      // Makes infowindow blank
       document.querySelector('#infowindow').innerHTML = ""
-      birds.NativeBirds.forEach(element => {
+
+      // Creates New Button for each Region 
+      info.Regions.forEach(element => {
       let newbutton = document.createElement('button')
       var Name = element.Name
       newbutton.innerHTML = Name
       newbutton.id = Name
-      newbutton.style.width = "10vw"
-      newbutton.style.height = "50px"
+      newbutton.style.width = "100vw"
+      newbutton.style.height = "33vh"
       document.querySelector('#infowindow').append(newbutton)
-      document.getElementById(element.Name).addEventListener('click', () => {
-        showbirdinfo(Name)
+
+      // Adapt this to add in button selection method  
+      // document.getElementById(element.Name).addEventListener('click', () => {
+        //showbirdinfo(Name)
+        
       })
       })
      }
      // Set Intial View
-      createbirdbuttons(birds);
+      setInitialView(info);
     })
+
 
