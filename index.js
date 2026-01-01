e19e77b6d95754b326f5c79cf0c36b5b86e76d84
TerraFantastic <ethen.pickett@gmail.com>
2026-01-01 20:00:39 +1300

    centre testing 2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ fetch("info.json")
         })
       
       //Update Map Centre and Visible Layers
-      document.querySelector("arcgis-map").center = "-40.900557, 174.885971"
+      document.querySelector("arcgis-map").centre = "-100.4593, 36.9014"
       } 
     
     // Function for displaying all regions
