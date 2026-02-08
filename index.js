8635fa0b67814757dd04f5180b2ded75d13c1774
TerraFantastic <ethen.pickett@gmail.com>
2026-02-08 16:09:14 +1300

    Added in link to web component

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import "@arcgis/map-components/components/arcgis-map";
+
 // Load in Json file 
 fetch("info.json")
   .then(response => response.json())
   .then(json => {
-
 
     console.log("Loaded in JSON file")
     
@@ -121,11 +122,13 @@ fetch("info.json")
         regionView(info);
       })
     }
+
     const map = new WebMap({
       portalItem: {
         id: "c70b1ec991f54cc7b65399623cfa0952"
       }
     })
+
     console.log(map.allLayers)
     initalView();
 
